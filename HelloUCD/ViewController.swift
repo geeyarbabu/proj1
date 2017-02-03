14b6c9a3db3370f886625e13babf4a27ef959d25
41690AppGroup <geeyar.kuttuwagopinath@ucdconnect.ie>
2017-02-03 00:55:57 +0000

    second try layout

--- HelloUCD/ViewController.swift
+++ HelloUCD/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var right_view: UIView!
     @IBOutlet weak var left_view: UIView!
-    
+    @IBOutlet weak var right_view: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
